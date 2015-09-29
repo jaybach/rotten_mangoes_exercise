5410f64457983321dbd692e204998ead63c33b3d
jaybach <dr.jaybach@gmail.com>
2015-09-29 01:21:42 +0000

    step 8 complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 RottenMangoes::Application.routes.draw do
+
+  get "users/new"
+  get "users/create"
   resources :movies
+
+  resources :users, only: [:new, :create]
+
 end
 
   # get "movies/index"

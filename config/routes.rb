0b87282bd4043fe5ef9e55bfa9de8d7533d19a9a
jaybach <dr.jaybach@gmail.com>
2015-09-29 19:02:12 +0000

    step 17 complete

--- config/routes.rb
+++ config/routes.rb
@@ -7,12 +7,13 @@ RottenMangoes::Application.routes.draw do
 
   resources :movies do
     resources :reviews, only: [:new, :create]
+  end
 
   resources :users, only: [:new, :create]
 
   resources :sessions, only: [:new, :create, :destroy]
 
-  # resources :sessions, only: [:new, :create, :destroy]
+  root to: 'movies#index'
 
 end
 

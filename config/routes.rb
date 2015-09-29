d1d366fe28107d918c92220762d2d3e496829f53
jaybach <dr.jaybach@gmail.com>
2015-09-29 04:03:34 +0000

    step 11 complete

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,9 @@ RottenMangoes::Application.routes.draw do
 
   resources :users, only: [:new, :create]
 
-  resources :sessions, only: [:new, :create]
+  resources :sessions, only: [:new, :create, :destroy]
+
+  # resources :sessions, only: [:new, :create, :destroy]
 
 end
 
